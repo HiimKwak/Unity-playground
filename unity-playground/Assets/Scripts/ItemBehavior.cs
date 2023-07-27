c63b7b651fa11f0d2eaa6e81ce231db9d58ae3a6
HiImConan <mindol0320@naver.com>
2023-07-27 16:29:27 +0900

    add movement(WASD, jump) on Player object

---
+++ unity-playground/Assets/Scripts/ItemBehavior.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBehavior : MonoBehaviour
+{
+  // Start is called before the first frame update
+  void OnCollisionEnter(Collision collision)
+  {
+    if (collision.gameObject.name == "Player")
+    {
+      Destroy(this.transform.gameObject);
+      Debug.Log("Item collected!");
+    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
