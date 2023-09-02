30d47c756eb1d434522ff429cb264e7f40640ca8
hiimkwak <mindol0320@naver.com>
2023-09-02 23:07:52 +0900

    Background rotation

--- flight/Assets/Scripts/Background.cs
+++ flight/Assets/Scripts/Background.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Background : MonoBehaviour
-{
-    void Start()
-    {
-        
-    }
-
+{ 
+    private float moveSpeed = 3f;
     void Update()
     {
-        
+        transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+        if (transform.position.y < -10) {
+            transform.position += new Vector3(0, 10 * 2f, 0);
+        }
     }
 }
