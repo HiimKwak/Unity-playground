b6a374079e93cd3f86f895ca27fd8342d5762014
hiimkwak <mindol0320@naver.com>
2023-09-04 17:40:50 +0900

    Add logic on the game

--- flight/Assets/Scripts/PipeSpawnScript.cs
+++ flight/Assets/Scripts/PipeSpawnScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PipeSpawnScript : MonoBehaviour
 {
     public GameObject Pipe;
-    public float spawnRate = 2;
+    public float spawnRate = 4;
     private float timer = 0;
 
     public float heightOffset = 10;
